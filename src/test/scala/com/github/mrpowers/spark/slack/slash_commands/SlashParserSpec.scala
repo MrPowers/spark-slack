dff46f53d7775df3f604460f0341d15521c96cdd
MrPowers <matthewkevinpowers@gmail.com>
2018-04-17 14:23:17 -0500

    Add a spec to illustrate how to fetch the user_id

--- src/test/scala/com/github/mrpowers/spark/slack/slash_commands/SlashParserSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/slack/slash_commands/SlashParserSpec.scala
@@ -24,4 +24,14 @@ class SlashParserSpec extends FunSpec {
 
   }
 
+  describe("user_id") {
+
+    it("gets the user_id") {
+      val json = """{"token":"gIkuvaNzQIHg97ATvDxqgjtO","team_id":"T0001","team_domain":"example","channel_id":"C2147483705","channel_name":"test","user_id":"U2147483697","user_name":"Steve","command":"/weather","text":"this is some stuff","response_url":"https://hooks.slack.com/commands/1234/5678"}"""
+      val p = new SlashParser(json)
+      assert(p.slashResponse.user_id === "U2147483697")
+    }
+
+  }
+
 }
